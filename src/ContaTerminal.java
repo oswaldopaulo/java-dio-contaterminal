65deb58906fd1f3f1e7706d678364b5970693398
oswaldopaulo <oswaldo.paulo@gmail.com>
2023-08-18 08:46:45 -0300

    ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,12 +1,49 @@
+import java.util.Scanner;
 public class ContaTerminal {
     private int numero;
     private String agencia, nome;
     private float saldo;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ContaTerminal conta = new ContaTerminal();
+        System.out.println("Por favor, digite o número da Agência !");
+        conta.setAgencia(scanner.next());
 
-    public ContaTerminal(int numero, String agencia, String nome, float saldo) {
+        System.out.println("Por favor, digite o número da Conta !");
+        conta.setAgencia(scanner.next());
+
+        System.out.println("Por favor, digite o número da Conta !");
+        conta.setNumero(scanner.nextInt());
+
+        System.out.println("Por favor, digite o nome !");
+        conta.setNome(scanner.next());
+
+        System.out.println("Por favor, digite o saldo !");
+        conta.setSaldo(scanner.nextFloat());
+
+
+        System.out.println("Olá " + conta.getNome() +", obrigado por criar uma conta em nosso banco, sua agência é " + conta.getAgencia() +", conta " + conta.getNumero() + " e seu saldo " + conta.getSaldo() + " já está disponível para saque");
+
+
+    }
+
+    public ContaTerminal() {
+
+    }
+
+    public void setNumero(int numero) {
         this.numero = numero;
+    }
+
+    public void setAgencia(String agencia) {
         this.agencia = agencia;
+    }
+
+    public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public void setSaldo(float saldo) {
         this.saldo = saldo;
     }
 
